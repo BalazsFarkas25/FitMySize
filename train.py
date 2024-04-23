33964958418b788285fb3b35579704e0e345516a
Balazs Farkas <balazsf308@gmail.com>
2024-04-23 10:17:41 +0200

    (train) epoch 100 training with Dataset 100

--- train.py
+++ train.py
@@ -4,6 +4,6 @@ from ultralytics import YOLO
 
 # pretrained model. This will serve as a base for my custom training
 model = YOLO('yolov8n-pose.pt')
-model.train(data='./data.yaml', epochs=1, imgsz=960, device='cpu')
+model.train(data='./data.yaml', epochs=100, imgsz=960, device='cpu')
 
 
